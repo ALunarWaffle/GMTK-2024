840e8a71b775a31ae99bbe50568237378ae7ea46
JoshH6538 <143372242+JoshH6538@users.noreply.github.com>
2024-08-18 02:03:28 -0700

    slimmed down destruction process. Added new house with proper hitbox and collision to allow player inside

--- Scripts/building.gd
+++ Scripts/building.gd
@@ -1,7 +1,7 @@
 extends AnimatableBody3D
 
 #@onready var ani = $AnimationPlayer
-@onready var shell = $CollisionShape3D/Shell
+@onready var shell = $Mesh
 
 @export var health = 1
 @export var minDamage = 1
@@ -9,7 +9,9 @@ extends AnimatableBody3D
 func take_damage(damage):
 	if (damage >= minDamage):
 		health -= damage
+		print(health)
 		if (health <= 0):
 			if shell!=null and shell.has_method("destroy"):
+				print("DESTROYING")
 				shell.destroy()
 			#ani.play("temp_crumble")
